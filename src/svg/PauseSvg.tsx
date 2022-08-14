12fc07fd1fe6f0605557f1f263449a6c551cfd11
Mirian Okradze <okradzemirian@gmail.com>
2022-08-14 16:22:22 +0400

    fix: volume and pause svgs

--- src/svg/PauseSvg.tsx
+++ src/svg/PauseSvg.tsx
@@ -1,6 +1,17 @@
 const PauseSvg = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg xmlns='http://www.w3.org/2000/svg' width={510} height={510} xmlSpace='preserve' {...props}>
-    <path d='M255 0C114.75 0 0 114.75 0 255s114.75 255 255 255 255-114.75 255-255S395.25 0 255 0zm-25.5 357h-51V153h51v204zm102 0h-51V153h51v204z' />
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='510'
+    height='510'
+    x='0'
+    y='0'
+    enableBackground='new 0 0 510 510'
+    version='1.1'
+    viewBox='0 0 510 510'
+    xmlSpace='preserve'
+    {...props}
+  >
+    <path d='M255 0C114.75 0 0 114.75 0 255s114.75 255 255 255 255-114.75 255-255S395.25 0 255 0zm-25.5 357h-51V153h51v204zm102 0h-51V153h51v204z'></path>
   </svg>
 )
 

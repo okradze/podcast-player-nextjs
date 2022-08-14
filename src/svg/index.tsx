4432f3ebcbff7506f3fc1b0500027ccb8409e02b
Mirian Okradze <okradzemirian@gmail.com>
2022-08-14 18:50:46 +0400

    refactor: fixtures file extension

--- src/svg/index.tsx
+++
@@ -1,6 +0,0 @@
-export { default as HomeSvg } from './HomeSvg'
-export { default as SearchSvg } from './SearchSvg'
-export { default as PodcastSvg } from './PodcastSvg'
-export { default as PlaySvg } from './PlaySvg'
-export { default as PauseSvg } from './PauseSvg'
-export { default as VolumeSvg } from './VolumeSvg'

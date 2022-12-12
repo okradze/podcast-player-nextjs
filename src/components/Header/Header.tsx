6e8f5e4c7ff156834423d4dbe83240fede39d643
Mirian Okradze <okradzemirian@gmail.com>
2022-12-12 23:30:11 +0400

    feat: add signin and signup links in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import useMe from '../../hooks/useMe'
 import ProfileButton from '../ProfileButton'
 import SearchBar from '../SearchBar'
 import styles from './Header.module.scss'
+import HeaderAuthLinks from './HeaderAuthLinks'
 
 const Header = () => {
   const me = useMe()
@@ -9,7 +10,7 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <SearchBar />
-      {me && <ProfileButton fullName={me.fullName} />}
+      {me ? <ProfileButton fullName={me.fullName} /> : <HeaderAuthLinks />}
     </header>
   )
 }

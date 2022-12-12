671d1a73135ce38b0c3da04b7f1ff760d4061b9d
Mirian Okradze <okradzemirian@gmail.com>
2022-12-12 23:08:37 +0400

    feat: design profile button dropdown

--- src/components/ProfileButton/ProfileButton.tsx
+++ src/components/ProfileButton/ProfileButton.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link'
+import SettingsIcon from '@mui/icons-material/Settings'
+import LogoutIcon from '@mui/icons-material/Logout'
 import styles from './ProfileButton.module.scss'
 
 type ProfileButtonProps = {
@@ -5,10 +8,30 @@ type ProfileButtonProps = {
 }
 
 export const ProfileButton = ({ fullName }: ProfileButtonProps) => (
-  <button className={styles.Button}>
-    <div className={styles.Avatar}>{fullName[0]}</div>
-    <p className={styles.Name}>{fullName}</p>
-  </button>
+  <section className={styles.section}>
+    <button className={styles.button}>
+      <div className={styles.avatar}>{fullName[0]}</div>
+      <p className={styles.name}>{fullName}</p>
+    </button>
+
+    <section className={styles.dropdown}>
+      <ul className={styles.dropdownList}>
+        <li>
+          <Link href='/settings'>
+            <a className={styles.dropdownButton} href=''>
+              <SettingsIcon className={styles.dropdownIcon} /> Settings
+            </a>
+          </Link>
+        </li>
+        <li>
+          <button className={styles.dropdownButton}>
+            <LogoutIcon className={styles.dropdownIcon} />
+            Sign Out
+          </button>
+        </li>
+      </ul>
+    </section>
+  </section>
 )
 
 export default ProfileButton

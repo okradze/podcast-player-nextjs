db92e7d310c0d61d1377bdd214f43fa15302a90c
Mirian Okradze <okradzemirian@gmail.com>
2022-12-25 15:38:32 +0400

    feat: add animation to podcast favorite button

--- src/components/PodcastFavoriteButton/PodcastFavoriteButton.tsx
+++ src/components/PodcastFavoriteButton/PodcastFavoriteButton.tsx
@@ -1,8 +1,10 @@
+import { SwitchTransition, CSSTransition } from 'react-transition-group'
 import useMe from '@/hooks/useMe'
 import useFavoritePodcast from '@/hooks/useFavoritePodcast'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 import FavoriteIcon from '@mui/icons-material/Favorite'
 import styles from './PodcastFavoriteButton.module.scss'
+import { useRef } from 'react'
 
 type PodcastFavoriteButtonProps = {
   isFavorite?: boolean
@@ -13,6 +15,10 @@ const PodcastFavoriteButton = ({ isFavorite, id }: PodcastFavoriteButtonProps) =
   const me = useMe()
   const { addOrRemoveFavorite } = useFavoritePodcast({ isFavorite, id })
 
+  const favoriteRef = useRef(null)
+  const notFavoriteRef = useRef(null)
+  const nodeRef = isFavorite ? favoriteRef : notFavoriteRef
+
   if (!me) return null
 
   return (
@@ -21,11 +27,26 @@ const PodcastFavoriteButton = ({ isFavorite, id }: PodcastFavoriteButtonProps) =
       aria-label={isFavorite ? 'Remove podcast from favorites' : 'Add podcast to favorites'}
       className={styles.button}
     >
-      {isFavorite ? (
-        <FavoriteIcon className={styles.icon} />
-      ) : (
-        <FavoriteBorderIcon className={styles.icon} />
-      )}
+      <SwitchTransition>
+        <CSSTransition
+          key={isFavorite ? 'remove' : 'add'}
+          timeout={200}
+          unmountOnExit
+          nodeRef={nodeRef}
+          classNames={{
+            enter: styles.ender,
+            exit: styles.exit,
+            enterActive: styles.enterActive,
+            exitActive: styles.exitActive,
+          }}
+        >
+          {isFavorite ? (
+            <FavoriteIcon ref={nodeRef} className={styles.icon} />
+          ) : (
+            <FavoriteBorderIcon ref={nodeRef} className={styles.icon} />
+          )}
+        </CSSTransition>
+      </SwitchTransition>
     </button>
   )
 }
